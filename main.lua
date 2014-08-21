1707a8d5c6c47dd9d22e6f6b3d3ba357be5f31c9
Fernando Moreno <fmorenoper@gmail.com>
2014-08-21 12:02:26 +0200

    some changes to work with last version of worona

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- ZERO BRANE STUDIO DEBUGGER
- require("mobdebug").start()
+ --require("mobdebug").start()
 
 -- WORONA INITIALIZATION
 local worona = require "worona"
@@ -10,10 +10,8 @@ worona.log:start( { level = "INFO", reset_log = true, console = true, file = "lo
 
 
 -- WORONA CONFIGURATION
-worona.app_url = "http://test.turismob.com"  --. Enter the url from which the app will read the data.
-
--- WORDPRESS URL
-worona.wp_url = "http://www.worona.org"
+worona.wp_url = "http://test.turismob.com"
+worona.current_style = "flat-ui"
 
 -- START THE APPLICATION
 worona:do_action( "load_style" )
